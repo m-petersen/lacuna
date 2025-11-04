76ba73442db81735f0425cbbeb0f77ec6fcb8bda
Marvin Petersen <marvinpetersen94@gmail.com>
2025-11-04 19:41:17 +0100

    Export batch_process in main package API

--- src/ldk/__init__.py
+++ src/ldk/__init__.py
@@ -10,6 +10,7 @@ __version__ = "0.1.0"
 # Core data structures
 # Bundled data access (convenience imports)
 from . import data
+from .batch import batch_process
 from .core.lesion_data import LesionData
 
 # Exports
@@ -17,4 +18,5 @@ __all__ = [
     "__version__",
     "LesionData",
     "data",
+    "batch_process",
 ]
