1eefcf7acbd347dffe96910ed93cb060cfdab5e4
Marvin Petersen <marvinpetersen94@gmail.com>
2025-11-04 19:41:29 +0100

    Add export utilities and BIDS validation

--- src/ldk/io/__init__.py
+++ src/ldk/io/__init__.py
@@ -6,12 +6,27 @@ Provides functions for:
 - Loading BIDS datasets
 - Exporting results to BIDS derivatives format
 - Saving NIfTI files
+- Exporting analysis results to CSV/TSV/JSON
 - Fetching and caching reference datasets (atlases, templates)
 - Converting connectome data to LDK format
 """
 
-from .bids import BidsError, export_bids_derivatives, load_bids_dataset, save_nifti
+from .bids import (
+    BidsError,
+    export_bids_derivatives,
+    load_bids_dataset,
+    save_nifti,
+    validate_bids_derivatives,
+)
 from .convert import gsp1000_to_ldk, trk_to_tck
+from .export import (
+    batch_export_to_csv,
+    batch_export_to_tsv,
+    export_provenance_to_json,
+    export_results_to_csv,
+    export_results_to_json,
+    export_results_to_tsv,
+)
 from .fetch import (
     get_atlas,
     get_connectome_path,
@@ -25,6 +40,13 @@ __all__ = [
     "load_bids_dataset",
     "export_bids_derivatives",
     "save_nifti",
+    "validate_bids_derivatives",
+    "export_results_to_csv",
+    "export_results_to_tsv",
+    "export_results_to_json",
+    "export_provenance_to_json",
+    "batch_export_to_csv",
+    "batch_export_to_tsv",
     "get_atlas",
     "get_connectome_path",
     "get_data_dir",
