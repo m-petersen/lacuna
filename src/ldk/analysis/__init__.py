4c45b88ba0c060cbaecc0f2146ae769b1f83492c
Marvin Petersen <marvinpetersen94@gmail.com>
2025-11-06 18:28:20 +0100

    feat(analysis): export FunctionalNetworkMapping in public API
    - Add FunctionalNetworkMapping to __all__ exports
    - Make fLNM analysis available at package level

--- src/ldk/analysis/__init__.py
+++ src/ldk/analysis/__init__.py
@@ -7,7 +7,14 @@ for processing lesion data.
 
 from ldk.analysis.atlas_aggregation import AtlasAggregation
 from ldk.analysis.base import BaseAnalysis
+from ldk.analysis.functional_network_mapping import FunctionalNetworkMapping
 from ldk.analysis.regional_damage import RegionalDamage
 from ldk.analysis.structural_network_mapping import StructuralNetworkMapping
 
-__all__ = ["BaseAnalysis", "AtlasAggregation", "RegionalDamage", "StructuralNetworkMapping"]
+__all__ = [
+    "BaseAnalysis",
+    "AtlasAggregation",
+    "FunctionalNetworkMapping",
+    "RegionalDamage",
+    "StructuralNetworkMapping",
+]
